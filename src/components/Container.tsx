345b90cac07af3e66e56b20d6c18fde0f00f1ed3
OceanIT <duongoc79@gmail.com>
2025-07-28 18:40:08 +0700

    first commit

---
+++ src/components/Container.tsx
@@ -0,0 +1,10 @@
+import type React from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+const Container = ({ children }: Props) => {
+  return <div className="max-w-7xl mx-auto">{children}</div>;
+};
+
+export default Container;
